0a8660c34d030bb4c671c49423cd479709edcee5
Lynx1820 <dkeren@seas.upenn.edu>
2019-06-03 12:34:30 -0400

    added evalutaion for single modeality typo2

--- evaluation.py
+++ evaluation.py
@@ -140,7 +140,7 @@ def main():
         word_dict = Magnitude(paths['word_magnitude'])
         evaluate('vis', word_dict, 'magnitude')
         evaluate('zs', word_dict, 'magnitude')
-        evaluate_all(eval_set_list, fused_dict, 'magnitude')
+        evaluate_all(eval_set_list, word_dict, 'magnitude')
     else:    
         word_dict = Magnitude('/data1/embeddings/pymagnitude/word.magnitude')
         if args.model == 'c_linear':        
