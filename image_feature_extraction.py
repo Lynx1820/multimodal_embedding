cb38308d8d2755e5258fbc92ccbc7a7b70e00165
Lynx1820 <dkeren@seas.upenn.edu>
2019-06-30 11:39:30 -0400

    added inference

--- image_feature_extraction.py
+++ image_feature_extraction.py
@@ -61,22 +61,23 @@ class SaveFeatures():
             self.features = np.row_stack((self.features, out))
     def remove(self): 
         self.hook.remove()
-
-def extract_features(train_data): 
-    bs = params.bs #batch size
+def train(train_df): 
 
     # default transformations applied below 
-    my_data = ImageDataBunch.from_df(paths['code_dir'], train_data, ds_tfms=get_transforms(), size=224, bs=bs, folder=paths['mmid_dir']).normalize(imagenet_stats)
+    my_data = ImageDataBunch.from_df(paths['code_dir'], train_df, ds_tfms=get_transforms(), size=224, bs=params.bs, folder=paths['mmid_dir']).normalize(imagenet_stats)
     learn = cnn_learner(my_data, models.resnet50, metrics=error_rate)
 
     #This changes the forwards layers of the model 
     learn.fit_one_cycle(params.train_epochs,params.lr)
-
-    #saving model 
+        #saving model 
     modelname = paths['data_dir'] + "/" + params.model_name
     print("Saving model to: " + modelname)
     learn.save(modelname)
+    return learn, my_data
 
+def extract_features(learn, my_data=None, train_df=None):
+    if my_data == None: 
+        my_data = ImageDataBunch.from_df(paths['code_dir'], train_df, ds_tfms=get_transforms(), size=224, bs=params.bs, folder=paths['mmid_dir']).normalize(imagenet_stats) 
     sf = SaveFeatures(learn.model[1][5]) 
 
     _= learn.get_preds(my_data.train_ds)
@@ -89,12 +90,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Image Feature Extraction')
     parser.add_argument("--config", type=str, default=None, help= 'config file to specify paths')
     parser.add_argument("--dict", type=str, nargs='+', default=None, help='List of image disctionaries to train on')
-    parser.add_argument("--fea_filename", type=str, default=None, help='Features\' filename')
+    parser.add_argument("--fea_filepath", type=str, default=None, help='Features\' filename')
     parser.add_argument("--workers", type=int, default=1, help="number of processes to do work in the distributed cluster")
     parser.add_argument("--pid", type=int, default=None, help="set to true when just extracting features" )
-    parser.add_argument("--mode",choices=['train', 'eval', 'partition', 'magnitude', 'build_df'], help="build_df: preprocess data and create embeddings, eval: evaluate embeddings")
+    parser.add_argument("--mode",choices=['train', 'eval', 'partition', 'magnitude', 'build_df', 'inference'], help="build_df: preprocess data and create embeddings, eval: evaluate embeddings")
     parser.add_argument("--train_epochs", type=int, default=8, help="number of epochs to train before extracting features") 
     parser.add_argument("--model_name", type=str, default="model_rn50", help="model name") 
+    parser.add_argument("--train_df", type=str, default=None, help="file for inteference")
     #Model Paramaters
     parser.add_argument("--bs", type=int, default=64, help="batch size for training") 
     parser.add_argument("--lr", type=float, default=1e-2, help="learning rate") 
@@ -104,26 +106,38 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read(params.config)
     paths = config['PATHS']
-    for dict_fn in params.dict:
-        assert os.path.isdir(paths['mmid_dir'] + "/" + dict_fn)
     ## TODO: assert for all partials workers
     assert os.path.isfile(paths['word_magnitude'])
-#    assert os.path.isfile('train_df.csv')
-    if params.mode == 'train': 
+    if params.mode == 'train':
+        for dict_fn in params.dict:
+            assert os.path.isdir(paths['mmid_dir'] + "/" + dict_fn) 
         # TODO: if everythings works then maybe don't save the file or delete
         df_list = []
         for dict_fn in params.dict: 
             df_list.append(pd.read_csv(dict_fn + '-train_df.csv', sep='\t', index_col=[0]))
         all_dfs = pd.concat(df_list).reset_index().drop(columns=['index'])
         all_dfs.to_csv("all-train_df.csv", sep='\t')
-        translations, features = extract_features(all_dfs)
-        filename = paths['data_dir'] + "/img_embeddings_resnet50-dicts" + str(len(params.dict)) + .txt"
+        translations, features = extract_features(train(all_dfs))
+        filename = paths['data_dir'] + "/img_embeddings_rn50-dicts" + str(len(params.dict)) + ".txt"
         print("Saving features to file: " + filename)
         with open(filename, 'a') as f:
             for word, arr in zip(translations,features):
                 f.write(word + "\t")
                 np.savetxt(f, arr.reshape(1,len(arr)), delimiter=' ')
         exit(0)
+    elif params.mode == 'inference':
+        print("Using modelname: " + params.model_name)
+        print("Inference on the following file" + params.train_df)
+        learn = cnn_learner(data, models.resnet50).load(params.model_name)
+        train_df = pd.read_csv(params.train_df) 
+        translations, features = extract_features(learn, None, train_df) 
+        filename = paths['data_dir'] + "/img_embeddings_rn50.txt"
+        print("Saving features to file: " + filename)
+        with open(filename, 'a') as f:
+            for word, arr in zip(translations,features):
+                f.write(word + "\t")
+                np.savetxt(f, arr.reshape(1,len(arr)), delimiter=' ')
+        print("Done saving")
     elif params.mode == 'build_df': 
         df_list = []
         for dict_fn in params.dict: 
@@ -134,7 +148,7 @@ if __name__ == '__main__':
         full_file = open(fn, 'w')
         words = Counter()
         print("Writing to %s", fn)
-        with open(filename, 'r') as file: 
+        with open(params.fea_filepath, 'r') as file: 
             for line in file:
                 word, emb = line.split('\t')
                 if words[word] == 0: 
